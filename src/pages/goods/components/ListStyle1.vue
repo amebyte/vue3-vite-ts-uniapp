8b7dd7248336970f9eef153a43d9c4c85aa1d5a1
unknown <416212316@qq.com>
2022-01-04 22:40:18 +0800

    add

--- src/pages/goods/components/ListStyle1.vue
+++ src/pages/goods/components/ListStyle1.vue
@@ -1,46 +1,75 @@
 <template>
   <view class="list">
-    <view class="item">
-      <view class="tag">限时特惠</view>
-      <view class="content">
-        <view class="image">
-          <image
-            src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
-            mode="scaleToFill"
-          ></image>
-        </view>
-        <view class="wrap">
-          <view class="label-wrap">
-            <view class="label">藤茶采制</view>
-            <view class="label r">线下课程</view>
+    <block v-for="(item, index) in newGoodsList" :key="index">
+      <view class="item">
+        <view class="tag">限时特惠</view>
+        <view class="content">
+          <view class="image">
+            <image
+              src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
+              mode="scaleToFill"
+            ></image>
           </view>
-          <view class="title">走进自然 茶韵飘香走进自然 茶韵飘香走进自然 茶韵飘香</view>
-          <view class="row">
-            <view class="label">授课老师：</view>
-            <view class="content">黄XX老师</view>
-          </view>
-          <view class="row">
-            <view class="label">开课时间：</view>
-            <view class="content">10月11日 上午10:00</view>
-          </view>
-        </view>
-      </view>
-      <view class="action">
-        <view class="l">已报名<text>3</text>人</view>
-        <view class="r">
-          <view class="btn">
-            <view class="l">
-              <text>¥199</text>
-              <text>¥199</text>
+          <view class="wrap">
+            <view class="label-wrap">
+              <view class="label">藤茶采制</view>
+              <view class="label r">线下课程</view>
             </view>
-            <view class="r"> 马上抢购 </view>
+            <view class="title">走进自然 茶韵飘香走进自然 茶韵飘香走进自然 茶韵飘香</view>
+            <view class="row">
+              <view class="label">授课老师：</view>
+              <view class="content">黄XX老师</view>
+            </view>
+            <view class="row">
+              <view class="label">开课时间：</view>
+              <view class="content">10月11日 上午10:00</view>
+            </view>
+          </view>
+        </view>
+        <view class="action">
+          <view class="l">已报名<text>3</text>人</view>
+          <view class="r">
+            <view class="btn">
+              <view class="l">
+                <text>¥199</text>
+                <text>¥199</text>
+              </view>
+              <view class="r"> 马上抢购 </view>
+            </view>
           </view>
         </view>
       </view>
-    </view>
+    </block>
   </view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onPageScroll, onLoad, onShow, onHide, onReachBottom } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import { fetchRecommendGoodsList } from '@/api/goods'
+type goodsType = {
+  id: string
+  pic: string
+  prodName: string
+  price: number
+}
+let newGoodsList = ref<Array<goodsType>>([])
+const getNewGoodsList = () => {
+  const params = {
+    pageNum: 0,
+    pageSize: 10,
+  }
+  fetchRecommendGoodsList(params)
+    .then((r) => {
+      console.log('r', r)
+      newGoodsList.value = r.data
+      console.log('newGoodsList', newGoodsList)
+    })
+    .catch((err) => console.log(err))
+}
+onLoad(() => {
+  getNewGoodsList()
+})
+</script>
 <style lang="scss" scoped>
 .list {
   margin: 20rpx;
@@ -52,6 +81,7 @@
     box-shadow: 0px 0px 14rpx 6rpx rgba(29, 233, 182, 0.11);
     border-radius: 20rpx;
     padding: 20rpx;
+    margin-bottom: 40rpx;
     .tag {
       position: absolute;
       top: 0;
