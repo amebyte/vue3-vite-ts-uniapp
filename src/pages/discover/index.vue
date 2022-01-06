8b419229bee39588967fc50b9e15d620f73b8c93
unknown <416212316@qq.com>
2022-01-06 22:05:45 +0800

    fix

--- src/pages/discover/index.vue
+++ src/pages/discover/index.vue
@@ -41,6 +41,59 @@
       </view>
     </view>
     <!--banner-nav end-->
+    <!--答疑列表 start-->
+    <view class="list">
+      <view class="item">
+        <view class="recommend"><text>精选</text></view>
+        <view class="user-info">
+          <view class="profile">
+            <view class="l">
+              <image
+                src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
+                mode="scaleToFill"
+              ></image>
+            </view>
+            <view class="r">
+              <view class="name">专家答疑</view>
+              <view class="desc">1小时前</view>
+            </view>
+          </view>
+          <view class="more">
+            <text class="iconfont icon-more"></text>
+          </view>
+        </view>
+        <view class="text-content">
+          专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑专家答疑
+        </view>
+        <view class="image-list">
+          <view class="image">
+            <image
+              src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
+              mode="scaleToFill"
+            ></image>
+          </view>
+          <view class="image">
+            <image
+              src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
+              mode="scaleToFill"
+            ></image>
+          </view>
+          <view class="image">
+            <image
+              src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
+              mode="scaleToFill"
+            ></image>
+          </view>
+          <view class="image">
+            <image
+              src="http://tstatic.mevikycloud.cn/image/product/2021/07/29/12051e1933364cd68168020f923e1a9fsjokznvbvn.jpg"
+              mode="scaleToFill"
+            ></image>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!--答疑列表 end-->
   </view>
 </template>
 
@@ -78,6 +131,7 @@ import { ref } from 'vue'
     }
   }
   .banner-nav {
+    margin-bottom: 40rpx;
     .content {
       display: flex;
       justify-content: space-between;
@@ -112,6 +166,98 @@ import { ref } from 'vue'
         }
         &:last-child {
           padding-right: 130rpx;
+        }
+      }
+    }
+  }
+  .list {
+    .item {
+      padding: 20rpx;
+      box-shadow: 0px 0px 14rpx 6rpx rgba(29, 233, 182, 0.11);
+      border-radius: 20rpx;
+      .recommend {
+        text {
+          background-color: #80c269;
+          border-radius: 7rpx;
+          font-size: 19rpx;
+          font-family: Hiragino Sans GB;
+          font-weight: bold;
+          font-style: italic;
+          color: #ffffff;
+          padding: 5rpx 10rpx;
+        }
+      }
+      .user-info {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .profile {
+          display: flex;
+          justify-content: flex-start;
+          padding: 30rpx 20rpx;
+          padding-left: 0;
+          padding-bottom: 20rpx;
+          > .l {
+            image {
+              width: 66rpx;
+              height: 66rpx;
+              border-radius: 50%;
+            }
+          }
+          > .r {
+            padding-left: 20rpx;
+            .name {
+              font-size: 28rpx;
+              font-family: PingFang SC;
+              font-weight: bold;
+              color: #80c269;
+              @extend .line1;
+            }
+            .desc {
+              font-size: 20rpx;
+              font-family: PingFang SC;
+              font-weight: bold;
+              color: #a6a6a6;
+              @extend .line1;
+            }
+          }
+        }
+        .more {
+          width: 40rpx;
+          overflow: hidden;
+          margin-left: 10rpx;
+          .icon-more {
+            font-size: 60rpx;
+            color: #bfbfbf;
+          }
+        }
+      }
+      .text-content {
+        font-size: 28rpx;
+        font-family: PingFang SC;
+        font-weight: 400;
+        color: #050505;
+        line-height: 40rpx;
+        padding-left: 20rpx;
+        padding-right: 20rpx;
+      }
+      .image-list {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        margin-top: 20rpx;
+        padding-left: 20rpx;
+        padding-right: 20rpx;
+        .image {
+          width: 193rpx;
+          height: 193rpx;
+          border-radius: 7rpx;
+          margin-bottom: 20rpx;
+          image {
+            width: 100%;
+            height: 100%;
+            border-radius: 7rpx;
+          }
         }
       }
     }
